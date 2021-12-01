5bd554a99b3cfecafdac42f007e0bbad4739c474
tbagriyanik <tbagriyanik@gmail.com>
2021-12-01 12:08:33 +0300

    ilk

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+	import Header from '$lib/header/Header.svelte';
+	import '../app.css';
+
+	import '../styles/tailwind-output.css';
+</script>
+
+<Header />
+
+<main>
+	<h1 class="uppercase text-indigo-500">Hello People of Earth</h1>
+	<slot />
+</main>
+
+<footer>
+	<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+</footer>
+
+<style>
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding: 1rem;
+		width: 100%;
+		max-width: 1024px;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+
+	footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 40px;
+	}
+
+	footer a {
+		font-weight: bold;
+	}
+
+	@media (min-width: 480px) {
+		footer {
+			padding: 40px 0;
+		}
+	}
+</style>
